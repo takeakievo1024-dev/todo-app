0aee28942a843a142b334cff5f8af0ddbda5976b
takenaka akihiro <take.aki.evo.1024@gmail.com>
2026-04-23 15:58:05 +0900

    add:入力フォーム追加

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$tx = $_POST["text"];
+echo $tx;
+
+?>
+
+<body>
+<form method = "POST" action = "index.php">
+    <input type = "text" name = "text">
+    <input type = "submit" value = "送信"><br>
+
+
+</body>
